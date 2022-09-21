036aeae844f37c1ef8c9beb9c933188f6a6ba0d3
hreinn <hreinngy@gmail.com>
2022-09-21 21:57:30 +0200

    Validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,40 @@
 import "../css/style.css"
-import {hello} from "./module.mjs";
 
-hello('Hello there')
+const formInputs = document.querySelectorAll('#form-inputs input')
+const email = document.querySelector('#email-address')
+const emailError = 'Email must be a valid noroff.no adress'
+const password = document.querySelector('#password')
+const passwordError = 'Password must 8 characters or more'
+const form = document.querySelector('form')
+
+formInputs.forEach((item) => {
+  item.onkeyup = function () {
+    this.classList.remove('bg-red-50')
+    this.nextElementSibling.innerHTML = ''
+  }
+})
+
+form.addEventListener('submit', function (event)  {
+  event.preventDefault()
+  if (validateString(email, email.value, checkNoroffEmail, null, emailError) && validateString(password, password.value, checkLength, 8, passwordError)) {
+    location.href = '../main.html'
+  }
+})
+
+function validateString(elem, value, callBack, length, errorMsg) {
+  if (!callBack(elem, length)) {
+    elem.classList.add('bg-red-50')
+    elem.nextElementSibling.innerHTML = errorMsg
+    return false
+  } else {
+    return true
+  }
+}
+
+function checkNoroffEmail(elem) {
+  return elem.value.endsWith('noroff.no')
+}
+
+function checkLength(elem, length) {
+  return elem.value.length >= length
+}
