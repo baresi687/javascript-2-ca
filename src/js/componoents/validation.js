2393df8e52583ffad85b338e0104cb598888b86b
hreinn <hreinngy@gmail.com>
2022-09-27 08:44:52 +0200

    Sign up done

---
+++ src/js/componoents/validation.js
@@ -0,0 +1,29 @@
+function checkName(elem) {
+  const regex = /^[\w]+$/
+  return regex.test(elem.value.trim())
+}
+
+function checkNoroffEmail(elem) {
+  const regex = /^[\w\-.]+@(stud.)?noroff.no$/
+  return regex.test(elem.value)
+}
+
+function checkLength(elem, length) {
+  return elem.value.trim().length >= length
+}
+
+function checkConfirmPassword(value, password) {
+  return value.value === password;
+}
+
+function validateString(elem, value, callBack, length, errorMsg) {
+  if (!callBack(elem, length)) {
+    elem.classList.add('bg-red-50')
+    elem.nextElementSibling.innerHTML = errorMsg
+    return false
+  } else {
+    return true
+  }
+}
+
+export {checkName, checkNoroffEmail, checkLength, checkConfirmPassword, validateString}
--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,31 @@
 import "../css/style.css"
+import {checkName, checkNoroffEmail, checkLength, checkConfirmPassword, validateString} from "./componoents/validation";
 
-const formInputs = document.querySelectorAll('#form-inputs input')
-const email = document.querySelector('#email-address')
-const emailError = 'Email must be a noroff.no or stud.noroff.no adress'
-const password = document.querySelector('#password')
-const passwordError = 'Password must 8 characters or more'
-const form = document.querySelector('form')
+if (document.querySelector('form')) {
 
-formInputs.forEach((item) => {
-  item.onkeyup = function () {
-    this.classList.remove('bg-red-50')
-    this.nextElementSibling.innerHTML = ''
-  }
-})
+  const formInputs = document.querySelectorAll('#form-inputs input')
 
-form.addEventListener('submit', function (event)  {
-  event.preventDefault()
-  if (validateString(email, email.value, checkNoroffEmail, null, emailError) && validateString(password, password.value, checkLength, 8, passwordError)) {
-    location.href = '../main.html'
-  }
-})
+  const signUpform = document.querySelector('form')
+  const name = document.querySelector('#name')
+  const nameError = 'Name must not contain punctuation symbols apart from underscore'
+  const email = document.querySelector('#email-address')
+  const emailError = 'Email must be a noroff.no or stud.noroff.no adress'
+  const password = document.querySelector('#password')
+  const passwordError = 'Password must 8 characters or more'
+  const confirmPassword = document.querySelector('#confirm-password')
+  const conFirmPasswordError = 'Confirmed password does not match password'
 
-function validateString(elem, value, callBack, length, errorMsg) {
-  if (!callBack(elem, length)) {
-    elem.classList.add('bg-red-50')
-    elem.nextElementSibling.innerHTML = errorMsg
-    return false
-  } else {
-    return true
-  }
-}
+  signUpform.addEventListener('submit', function (event)  {
+    event.preventDefault()
+    if (validateString(name, name.value, checkName,null, nameError) && validateString(email, email.value, checkNoroffEmail, null, emailError) && validateString(password, password.value, checkLength, 8, passwordError) && validateString(confirmPassword, confirmPassword.value, checkConfirmPassword, password.value, conFirmPasswordError)) {
+      location.href = '../main.html'
+    }
+  })
 
-function checkNoroffEmail(elem) {
-  const regex = /^[\w\-.]+@(stud.)?noroff.no$/
-  return regex.test(elem.value)
-}
-
-function checkName(elem) {
-  const regex = /^[\w]+$/
-  return regex.test(elem.trim())
-}
-
-console.log(checkName(' hesAf_ddsfsd'));
-
-function checkLength(elem, length) {
-  return elem.value.length >= length
+  formInputs.forEach((item) => {
+    item.onkeyup = function () {
+      this.classList.remove('bg-red-50')
+      this.nextElementSibling.innerHTML = ''
+    }
+  })
 }
