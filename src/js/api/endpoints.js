28a57c157be95b09881780ee97a5135c9ba55cef
hreinn <hreinngy@gmail.com>
2022-10-01 12:41:39 +0200

    User posts progressing

--- src/js/api/endpoints.js
+++ src/js/api/endpoints.js
@@ -1,7 +1,10 @@
+import {name} from "../withToken";
+
 const API_BASE_URL = 'https://nf-api.onrender.com/api/v1/social'
 const apiSignUp = '/auth/register'
 const apiLogin = '/auth/login'
 const apiGetPosts = '/posts?_author=true&limit=12'
 const apiCreatePost = '/posts'
+const apiGetUserPosts = `/profiles/${name}?_posts=true`
 
-export {API_BASE_URL, apiSignUp, apiLogin, apiGetPosts, apiCreatePost}
+export {API_BASE_URL, apiSignUp, apiLogin, apiGetPosts, apiCreatePost, apiGetUserPosts}
---
+++ src/js/userPosts.js
@@ -0,0 +1,3 @@
+import {API_BASE_URL, apiGetUserPosts} from "./api/endpoints";
+
+console.log(API_BASE_URL+apiGetUserPosts);
