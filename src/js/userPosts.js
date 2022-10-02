1bc1ee9ba57f0d38614284db0885bbefc59fa2a9
hreinn <hreinngy@gmail.com>
2022-10-02 23:30:25 +0200

    Edit posts now working. Code needs cleanup

--- src/js/userPosts.js
+++ src/js/userPosts.js
@@ -4,6 +4,7 @@ import {formatDateLong} from "./utils/dateFormat";
 import {showErrorMsg} from "./utils/validation";
 
 const userPostsContainer = document.querySelector('#user-posts-container')
+const postEditModal = document.querySelector('#modal')
 const token = getFromStorage('accessToken')
 let postId = ''
 let apiDeletePost = API_BASE_URL + `/posts/`
@@ -30,12 +31,12 @@ async function getUserPosts(url) {
         const dateFormat = formatDateLong(created)
         return `<div class="flex justify-between p-6 my-2 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                   <div class="max-w-md">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${title}</h5>
+                    <h5 id="post-title-${id}" class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${title}</h5>
                     <small class="font-normal text-gray-700 dark:text-gray-300 mb-1">By <span class="font-bold">${owner}</span> on ${dateFormat}</small>
-                    <p class="py-4 font-normal text-gray-900 dark:text-white mb-1 whitespace-pre">${body}</p>
+                    <p id="post-body-${id}" class="py-4 font-normal text-gray-900 dark:text-white mb-1 whitespace-pre">${body}</p>
                   </div>
                   <div>
-                    <button id="edit-post" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</button>
+                    <button id="edit-post-${id}" class="edit-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</button>
                     <button id="delete-post-id-${id}" class="delete-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Delete</button>
                   </div>
                 </div>`
@@ -56,10 +57,61 @@ async function getUserPosts(url) {
         deletePost(apiDeletePost)
       }
     })
+    const editBtn = document.querySelectorAll('.edit-btn')
+    editBtn.forEach((btn) => {
+      btn.onclick = function (event) {
+        postEditModal.classList.remove('hidden')
+        console.log(this.id);
+        postId = this.id.substring(10)
+        console.log(postId);
+        const postTitle = document.querySelector(`#post-title-${postId}`).textContent
+        const postBody = document.querySelector(`#post-body-${postId}`).textContent
+        document.querySelector('#post-title').value = postTitle
+        document.querySelector('#post-body').value = postBody
+
+        const API_EDIT_POST = API_BASE_URL+'/posts/'+postId
+        console.log(API_EDIT_POST);
+
+        const editPostBtn = document.querySelector('#edit-post-btn')
+        editPostBtn.onclick = function () {
+          const putData = {
+            title: document.querySelector('#post-title').value,
+            body: document.querySelector('#post-body').value,
+          }
+          console.log(document.querySelector('#post-body').value);
+          editPost(API_EDIT_POST, putData)
+        }
+      }
+    })
+  }
+}
+
+window.onclick = function (event) {
+  if (event.target === postEditModal) {
+    postEditModal.classList.add('hidden')
   }
 }
 
 getUserPosts(API_BASE_URL+apiGetUserPosts)
+
+async function editPost(url, putData) {
+  try {
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify(putData)
+    };
+    const response = await fetch(url, options)
+    await response.json()
+    location.reload()
+
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 async function deletePost(url) {
   try {
