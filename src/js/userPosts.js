588d642a3f7b620e057a41715e67977409080b22
hreinn <hreinngy@gmail.com>
2022-10-04 15:00:38 +0200

    Switched edit and delete behaviour to call the user posts function, instead of reloading the page

--- src/js/userPosts.js
+++ src/js/userPosts.js
@@ -9,6 +9,7 @@ const token = getFromStorage('accessToken')
 let postId = ''
 
 async function getUserPosts(url) {
+  userPostsContainer.innerHTML = ''
   try  {
     const options = {
       method: 'GET',
@@ -25,20 +26,21 @@ async function getUserPosts(url) {
           `<h2 class="text-1.5xl">Nothing here</h2>
            <p class="my-4">Why not <a class="font-semibold underline hover:text-blue-800 dark:hover:text-blue-900" href="../create-post.html">Create a Post?</a></p>`
     } else {
-      const posts = data.map(({title, body, id, owner, created}) => {
-        const dateFormat = formatDateLong(created)
-        return `<div class="flex justify-between p-6 my-2 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                  <div class="max-w-md">
-                    <h5 id="post-title-${id}" class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${title}</h5>
-                    <small class="font-normal text-gray-700 dark:text-gray-300 mb-1">By <span class="font-bold">${owner}</span> on ${dateFormat}</small>
-                    <p id="post-body-${id}" class="py-4 font-normal text-gray-900 dark:text-white mb-1 whitespace-pre-line">${body}</p>
-                  </div>
-                  <div>
-                    <button data-id="${id}" class="edit-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</button>
-                    <button data-id="${id}" class="delete-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-700 dark:hover:bg-red-600 focus:outline-none dark:focus:ring-blue-800">Delete</button>
-                  </div>
-                </div>`
-      })
+      const posts = data
+          .map(({title, body, id, owner, created}) => {
+            const dateFormat = formatDateLong(created)
+            return `<div class="flex justify-between p-6 my-2 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                      <div class="max-w-md">
+                        <h5 id="post-title-${id}" class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${title}</h5>
+                        <small class="font-normal text-gray-700 dark:text-gray-300 mb-1">By <span class="font-bold">${owner}</span> on ${dateFormat}</small>
+                        <p id="post-body-${id}" class="py-4 font-normal text-gray-900 dark:text-white mb-1 whitespace-pre-line">${body}</p>
+                      </div>
+                      <div>
+                        <button data-id="${id}" class="edit-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</button>
+                        <button data-id="${id}" class="delete-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-700 dark:hover:bg-red-600 focus:outline-none dark:focus:ring-blue-800">Delete</button>
+                      </div>
+                    </div>`
+          }).reverse()
 
       userPostsContainer.innerHTML += posts.join('')
     }
@@ -65,6 +67,7 @@ async function getUserPosts(url) {
             body: document.querySelector('#post-body').value,
           }
           editPost(`${EDIT_DELETE_USER_POST}${postId}`, putData)
+          postEditModal.classList.add('hidden')
         }
       }
     })
@@ -99,7 +102,7 @@ async function editPost(url, putData) {
     const response = await fetch(url, options)
     if (response.ok) {
       await response.json()
-      location.reload()
+      getUserPosts(GET_USER_POSTS_URL)
     } else {
       showErrorMsg(document.querySelector('#general-error-edit'))
     }
@@ -120,7 +123,7 @@ async function deletePost(url) {
     const response = await fetch(url, options)
     if (response.ok) {
       await response.json()
-      location.reload()
+      getUserPosts(GET_USER_POSTS_URL)
     } else {
       showErrorMsg(document.querySelector('#general-error'))
     }
