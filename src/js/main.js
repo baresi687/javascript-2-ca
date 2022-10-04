ab8194f86dd211da23087fc33ccc5513e8d7255d
hreinn <hreinngy@gmail.com>
2022-10-04 22:13:32 +0200

    Added overflow-y-scroll to pages with token, improved responsive design on main posts page and filtered out posts with no titles

--- src/js/main.js
+++ src/js/main.js
@@ -17,14 +17,16 @@ async function getPosts(url) {
     const response = await fetch(url, options)
     const responseJSON = await response.json()
 
-    const data = responseJSON.map(({id,title, author, created}) => {
-      const dateFormat = formatDate(created)
-      return `<a href="./post-details?id=${id}" class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${title}</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-300 mb-1">By ${author.name}</p>
-                <p class="font-normal text-gray-700 dark:text-gray-300 mb-1">On ${dateFormat}</p>
-              </a>`
-    })
+    const data = responseJSON
+        .filter(({title}) => title)
+        .map(({id,title, author, created}) => {
+          const dateFormat = formatDate(created)
+          return `<a href="/post-details?id=${id}" class="basis-full p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${title}</h5>
+                    <p class="font-normal text-gray-700 dark:text-gray-300 mb-1">By ${author.name}</p>
+                    <p class="font-normal text-gray-700 dark:text-gray-300 mb-1">On ${dateFormat}</p>
+                  </a>`
+        })
 
     postsContainer.innerHTML += data.join('')
   } catch (error) {
