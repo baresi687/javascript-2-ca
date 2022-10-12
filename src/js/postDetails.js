d2d6278b453f39f8e0f15768dff11d5a1a72a9c8
hreinn <hreinngy@gmail.com>
2022-10-12 23:05:25 +0200

    Posting and seeing comments on post details page now working. Need to clean up html

--- src/js/postDetails.js
+++ src/js/postDetails.js
@@ -6,7 +6,20 @@ import {showErrorMsg} from "./utils/validation";
 const postId = new URLSearchParams(window.location.search).get('id')
 const postDetailsContainer = document.querySelector('#post-details-container')
 const titleOfPage = document.querySelector('title')
+const commentsContainer = document.querySelector('#comments-container')
+const commentForm = document.querySelector('#post-comment')
+const commentAsUser = document.querySelector('#comment-as')
 const token = getFromStorage('accessToken')
+const {name} = getFromStorage('userKey')
+
+commentForm.onsubmit = function (event) {
+  event.preventDefault()
+  const commentBody = document.querySelector('#comment-body')
+  const postData = {
+    body: commentBody.value
+  }
+  commentPost(`${GET_POST_DETAILS}${postId}/comment`, postData)
+}
 
 async function getPostDetails(url) {
   try {
@@ -20,7 +33,7 @@ async function getPostDetails(url) {
 
     if (response.ok) {
       const responseJSON = await response.json()
-      const {title, author, created, body, tags, media} = responseJSON
+      const {title, author, created, body, tags, media, comments} = responseJSON
       titleOfPage.innerHTML += title
       const dateFormat = formatDateLong(created)
 
@@ -43,7 +56,29 @@ async function getPostDetails(url) {
       tags.length ?
         postDetailsContainer.querySelector('#post-content-container').insertAdjacentHTML('beforeend',tagsHtml) : null
 
+      commentAsUser.innerHTML += name
+      commentsContainer.innerHTML = ''
+
+      if (comments.length) {
+        commentsContainer.innerHTML += `<h6 class="my-6 text-lg dark:text-black">Comments</h6>`
+        const commentsAvailable = comments.map(({owner, body}) => {
+          return `
+           <div class="p-6 my-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+             <div class="block xl:flex justify-between w-full gap-16">
+               <div class="basis-1/2 flex-grow">                 
+                 <small class="font-normal text-gray-700 dark:text-gray-300 mb-1">By <span class="font-bold">${owner}</span> on ${dateFormat}</small>
+                 <p class="py-4 font-normal text-gray-900 dark:text-white mb-1 whitespace-pre-line">${body}</p>
+               </div>                                                
+             </div>                      
+           </div>`
+        }).join('')
+
+        commentsContainer.classList.remove('hidden')
+        commentsContainer.innerHTML += commentsAvailable
+      }
+
     } else {
+      commentForm.classList.add('hidden')
       showErrorMsg(document.querySelector('#general-error'))
     }
 
@@ -52,4 +87,27 @@ async function getPostDetails(url) {
   }
 }
 
-getPostDetails(`${GET_POST_DETAILS}${postId}?_author=true`)
+async function commentPost(url, postData) {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify(postData)
+    };
+
+    const response = await fetch(url, options)
+
+    if (response.ok) {
+      getPostDetails(`${GET_POST_DETAILS}${postId}?_author=true&_comments=true&_reactions=true`)
+    } else {
+      showErrorMsg(document.querySelector('#general-error-comment'))
+    }
+  } catch (error) {
+    showErrorMsg(document.querySelector('#general-error-comment'))
+  }
+}
+
+getPostDetails(`${GET_POST_DETAILS}${postId}?_author=true&_comments=true&_reactions=true`)
