dd8dca0a3cc0f562ed3f83d50afe50389312bda8
hreinn <hreinngy@gmail.com>
2022-10-17 10:20:02 +0200

    Added build inputs

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default {
     rollupOptions: {
       input: {
         index: path.resolve(__dirname, './src/index.html'),
+        login: path.resolve(__dirname, './src/login.html'),
         main: path.resolve(__dirname, './src/main.html'),
+        postDetails: path.resolve(__dirname, './src/post-details.html'),
+        createPost: path.resolve(__dirname, './src/create-post.html'),
+        userPosts: path.resolve(__dirname, './src/user-posts.html'),
       },
     },
     outDir: '../dist',
